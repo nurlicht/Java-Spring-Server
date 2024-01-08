c5aaf370fa57e5f47b03d5ec69e6d31a3bf8df88
nurlicht <saman@gatech.edu>
2024-01-08 07:16:15 +0100

    Use the original key-type

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, String> {
 }
