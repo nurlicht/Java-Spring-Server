7b25fac24d0b5d969b4ba197700385fb15c8a3b0
Aliakbar Jafarpour <saman@gatech.edu>
2024-04-06 14:08:59 +0200

    Upgrade spring-ai

--- src/main/java/com/example/demo/controller/OpenAiController.java
+++ src/main/java/com/example/demo/controller/OpenAiController.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.Generation;
-import org.springframework.ai.openai.client.OpenAiChatClient;
-import org.springframework.ai.prompt.Prompt;
-import org.springframework.ai.prompt.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +31,7 @@ public class OpenAiController {
         log.info("prompt={}.", prompt);
         ChatResponse chatResponse;
         try {
-            chatResponse = openAiChatClient.generateStream(prompt).blockFirst();
+            chatResponse = openAiChatClient.stream(prompt).blockFirst();
         } catch (final WebClientResponseException e) {
             chatResponse = new ChatResponse(List.of(new Generation(e.getMessage())));
         }
